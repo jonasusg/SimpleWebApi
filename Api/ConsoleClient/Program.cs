e903133c50b35a628a2980ff0f4198a71cf6ddc9
Jonas Umaras <juma@stormgeo.com>
2023-06-06 11:55:46 +0300

    Create console client for API

---
+++ Api/ConsoleClient/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var httpClient = new HttpClient();
+
+var response = await httpClient.GetStringAsync("http://localhost:5000/WeatherForecast");
+
+var response2 = await httpClient.GetStringAsync("http://localhost:5000/WeatherForecast");
+
+Console.WriteLine(response);
